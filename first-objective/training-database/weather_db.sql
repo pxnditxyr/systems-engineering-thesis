fe0a701d17e1e3fdf7ac2adb16d54fd3cf2992ed
Pxndxs 🐼 <pxnditxyr@gmail.com>
2024-08-08 20:01:02 -0400

    feat(docker): add Docker Compose for database creation 🐳🗄️🚀 refactor(database): modify and improve weather database 🗄️🌦️🛠️ feat: create base for neural network proposal 🧠📊🔧

--- first-objective/training-database/weather_db.sql
+++ first-objective/training-database/weather_db.sql
@@ -1,7 +1,4 @@
 -- This is a database for training a transformer model to predict el nino events
-
-CREATE DATABASE IF NOT EXISTS weather_db;
-
 -- Table: locations
 
 CREATE TABLE IF NOT EXISTS locations (
@@ -10,86 +7,102 @@ CREATE TABLE IF NOT EXISTS locations (
   latitude  FLOAT NOT NULL,
   longitude FLOAT NOT NULL,
 
-  createdAT TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  updatedAT TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+  createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updatedAt TIMESTAMP
 );
 
 -- Table: precipitations
 
 CREATE TABLE IF NOT EXISTS precipitations (
   id          SERIAL PRIMARY KEY,
-  location_id INTEGER NOT NULL,
+  locationId  INTEGER NOT NULL,
   date        DATE NOT NULL,
   value       FLOAT NOT NULL,
 
-  createdAT TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  updatedAT TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  FOREIGN KEY ( location_id ) REFERENCES locations( id )
+  createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updatedAt TIMESTAMP,
+  FOREIGN KEY ( locationId ) REFERENCES locations( id )
 );
 
 -- Table: sea_surface_temperatures
 
 CREATE TABLE IF NOT EXISTS sea_surface_temperatures (
   id          SERIAL PRIMARY KEY,
-  location_id INTEGER NOT NULL,
+  locationId  INTEGER NOT NULL,
   date        DATE NOT NULL,
   value       FLOAT NOT NULL,
 
-  createdAT TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  updatedAT TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  FOREIGN KEY ( location_id ) REFERENCES locations( id )
+  createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updatedAt TIMESTAMP,
+  FOREIGN KEY ( locationId ) REFERENCES locations( id )
 );
 
 -- Table: air_temperatures
 
 CREATE TABLE IF NOT EXISTS air_temperatures (
   id          SERIAL PRIMARY KEY,
-  location_id INTEGER NOT NULL,
+  locationId  INTEGER NOT NULL,
   date        DATE NOT NULL,
   value       FLOAT NOT NULL,
 
-  createdAT TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  updatedAT TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  FOREIGN KEY ( location_id ) REFERENCES locations( id )
+  createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updatedAt TIMESTAMP,
+  FOREIGN KEY ( locationId ) REFERENCES locations( id )
 );
 
 -- Table: wind_speeds
 
 CREATE TABLE IF NOT EXISTS wind_speeds (
   id          SERIAL PRIMARY KEY,
-  location_id INTEGER NOT NULL,
+  locationId  INTEGER NOT NULL,
   date        DATE NOT NULL,
   value       FLOAT NOT NULL,
 
-  createdAT TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  updatedAT TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  FOREIGN KEY ( location_id ) REFERENCES locations( id )
+  createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updatedAt TIMESTAMP,
+  FOREIGN KEY ( locationId ) REFERENCES locations( id )
 );
 
 -- Table: weather
 
 CREATE TABLE IF NOT EXISTS weather (
   id                      SERIAL PRIMARY KEY,
-  location_id             INTEGER NOT NULL,
+  locationId              INTEGER NOT NULL,
   date                    DATE NOT NULL,
   precipitation           FLOAT NOT NULL,
   sea_surface_temperature FLOAT NOT NULL,
   air_temperature         FLOAT NOT NULL,
 
-  createdAT TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  updatedAT TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  FOREIGN KEY ( location_id ) REFERENCES locations( id )
+  createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updatedAt TIMESTAMP,
+  FOREIGN KEY ( locationId ) REFERENCES locations( id )
 );
 
 -- Table: incidents
 
 CREATE TABLE IF NOT EXISTS incidents (
   id          SERIAL PRIMARY KEY,
-  location_id INTEGER NOT NULL,
+  locationId  INTEGER NOT NULL,
   description TEXT NOT NULL,
   type        VARCHAR( 255 ) NOT NULL,
 
-  createdAT TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  updatedAT TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  FOREIGN KEY ( location_id ) REFERENCES locations( id )
+  createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updatedAt TIMESTAMP,
+  FOREIGN KEY ( locationId ) REFERENCES locations( id )
 );
+
+-- Triggers for updatedAt
+
+-- CREATE OR REPLACE FUNCTION update_timestamp()
+-- RETURNS TRIGGER AS $$
+-- BEGIN
+--   NEW.updatedAt = CURRENT_TIMESTAMP;
+--   RETURN NEW;
+-- END;
+-- $$ LANGUAGE plpgsql;
+--
+-- CREATE TRIGGER update_locations
+-- BEFORE UPDATE ON locations
+-- FOR EACH ROW
+-- EXECUTE FUNCTION update_timestamp();
+--
